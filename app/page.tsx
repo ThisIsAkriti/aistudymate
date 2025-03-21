50442f0c0e5a82423bcf1eccd44a382bebeb1f50
akriti <akritikohli05@gmail.com>
2025-03-21 19:53:41 +0530

    updated globals css

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "../components/ui/button"
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] text-3xl">
-      Your Ai Studymate
+      <h1>Your Ai Studymate</h1>
       <Button className="cursor-pointer" variant="outline" size={"lg"}>Hello</Button>
     </div>
   );
