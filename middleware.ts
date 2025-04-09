c9e5d2aa46b9513a727014835ff213cae6304497
akriti <akritikohli05@gmail.com>
2025-04-09 21:05:03 +0530

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,21 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // Define public routes that don't require authentication
-const isPublicRoute = createRouteMatcher([
+const isProtectedRoute = createRouteMatcher([
   "/",
-  "/sign-in",
-  "/sign-up",
   "/api/webhooks/clerk"
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isPublicRoute(req)) {
-    return;
-  }
-
   const { userId } = await auth();
-  
-  if (!userId) {
-    return new Response("Unauthorized", { status: 401 });
+  if (!userId && isProtectedRoute(req)) {
+      const { redirectToSignIn } = await auth();
+      return redirectToSignIn();
   }
-  
 });
-
 export const config = {
   matcher: [
-   "/((?!.+\\.[\\w]+$|_next).*)", 
-    "/(api|trpc)(.*)", // Protect API routes
-    "/", 
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/(api|trpc)(.*)", // Protect API route 
   ],
 };
